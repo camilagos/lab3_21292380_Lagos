36b69d22a47a58f23b60c48c59c5f7475924bd55
Camila Lagos <118062775+camilagos@users.noreply.github.com>
2023-12-01 23:14:34 -0300

    Modificación de interface

--- Codigo_Fuente_21292380_LagosLagos/src/main/java/org/example/OptionComportamiento_21292380_LagosLagos.java
+++ Codigo_Fuente_21292380_LagosLagos/src/main/java/org/example/OptionComportamiento_21292380_LagosLagos.java
@@ -1,4 +1,10 @@
 package org.example;
 
+import java.util.List;
+
 public interface OptionComportamiento_21292380_LagosLagos {
+    int getCode();
+    int getChatbotCodeLink();
+    int getInitialFlowCodeLink();
+    List<String> getKeywords();
 }
